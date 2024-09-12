86aad505f1f91453062d4ced4de506bfa6f09ce7
brent hartshorn <brenthartshorn@proton.me>
2024-09-12 15:59:00 -0700

    INKSCAPE_MODULE
    In file included from /usr/include/gdkmm-3.0/gdkmm.h:26,
                     from /usr/include/gtkmm-3.0/gtkmm.h:105,
                     from ./src/inkscape-application.h:23,
                     from /tmp/libinkscape.c++:2:
    /usr/include/gdkmm-3.0/gdkmm/window.h:2714:67: error: ‘Cairo::Format’ has not been declared
     2714 |   ::Cairo::RefPtr< ::Cairo::Surface> create_similar_image_surface(::Cairo::Format format, int width, int height, int scale);

--- inkscape.py
+++ inkscape.py
@@ -6,7 +6,7 @@ if '--install' in sys.argv:
 	if os.path.isfile('/usr/bin/dnf'):
 		cmd = 'sudo dnf install libsoup-devel gsl-devel pango-devel cairo-devel double-conversion-devel gc-devel potrace-devel gtkmm3.0-devel libgdl-devel gtkspell3-devel boost-devel libxslt-devel'
 	else:
-		cmd = 'sudo apt-get install libmagick++-dev libaspell-dev libgraphicsmagick1-dev libgtkspell3-3-dev libcdr-dev libvisio-dev libwpg-dev libwpd-dev libsoup2.4-dev libxslt-dev libboost-all-dev liblcms2-dev libgc-dev libdouble-conversion-dev libpotrace-dev libpangomm-2.48-dev libcairomm-1.16-dev libgtkmm-3.0-dev libgdl-3-dev libpoppler-dev libpoppler-glib-dev mm-common'
+		cmd = 'sudo apt-get install libharfbuzz-dev libmagick++-dev libaspell-dev libgraphicsmagick1-dev libgtkspell3-3-dev libcdr-dev libvisio-dev libwpg-dev libwpd-dev libsoup2.4-dev libxslt-dev libboost-all-dev liblcms2-dev libgc-dev libdouble-conversion-dev libpotrace-dev libpangomm-2.48-dev libcairomm-1.16-dev libgtkmm-3.0-dev libgdl-3-dev libpoppler-dev libpoppler-glib-dev mm-common'
 	print(cmd)
 	subprocess.check_call(cmd.split())
 
@@ -67,9 +67,55 @@ def ensure_user_config( minimal=True ):
 	else:
 		open(os.path.join(iui, 'toolbar-tool.ui'), 'w').write(open('./share/ui/toolbar-tool.ui').read())
 
+INKSCAPE_MODULE = '''
+#include "inkscape-application.h"
+InkscapeApplication *app
+
+extern "C" int inkscape_init(){
+	if (gtk_init_check(NULL, NULL)){
+		app = new InkscapeApplication();
+		return 1;
+	} else {
+		return 0;
+	}
+}
+'''
 
 def inkscape_python():
-	lib = ctypes.CDLL(INKSCAPE_SO)
+	so  = '/tmp/libinkscape.so'
+	tmp = '/tmp/libinkscape.c++'
+	open(tmp,'w').write(INKSCAPE_MODULE)
+	cmd = [
+		'g++', '-o', so, tmp, '-l', INKSCAPE_SO, 
+		'-I./src/include', '-I./src', '-I/usr/include/glib-2.0',
+		'-I/usr/include/giomm-2.4', 
+		#'-I/usr/include/gdkmm-2.4',
+		'-I/usr/include/gdkmm-3.0',
+		'-I/usr/include/pangomm-2.48', '-I/usr/include/pango-1.0',
+		'-I/usr/include/harfbuzz', '-I/usr/include/cairomm-1.16',
+		'-I/usr/include/cairo', '-I/usr/include/freetype2',
+		'-I/usr/include/gtk-3.0',
+		'-I/usr/include/gdk-pixbuf-2.0',
+		'-I/usr/include/atkmm-1.6',
+		'-I/usr/include/atk-1.0',
+	]
+	cmd += [
+		'-I/usr/include/gtkmm-3.0/', '-I/usr/include/glibmm-2.4', 
+		'-I/usr/lib/x86_64-linux-gnu/glibmm-2.4/include', '-I/usr/lib/x86_64-linux-gnu/glib-2.0/include',
+		'-I/usr/include/sigc++-2.0/', '-I/usr/lib/x86_64-linux-gnu/sigc++-2.0/include',
+		'-I/usr/lib/x86_64-linux-gnu/giomm-2.4/include',
+
+		#'-I/usr/lib/x86_64-linux-gnu/gdkmm-2.4/include',
+		'-I/usr/lib/x86_64-linux-gnu/gdkmm-3.0/include',
+
+		'-I/usr/lib/x86_64-linux-gnu/pangomm-2.48/include',
+		'-I/usr/lib/x86_64-linux-gnu/cairomm-1.16/include',
+		'-I/usr/lib/x86_64-linux-gnu/gtkmm-3.0/include',
+		'-I/usr/lib/x86_64-linux-gnu/atkmm-1.6/include',
+		]
+	print(cmd)
+	subprocess.check_call(cmd)
+	lib = ctypes.CDLL(so)
 	print(lib)
 
 if __name__=='__main__':
