b630bef35290855eb14b20d8f635dad9df69c4e2
brent hartshorn <brenthartshorn@proton.me>
2024-09-21 10:38:41 -0700

    ubuntu install fixes

--- inkscape.py
+++ inkscape.py
@@ -7,7 +7,7 @@ if '--install' in sys.argv:
 	if os.path.isfile('/usr/bin/dnf'):
 		cmd = 'sudo dnf install python3-gobject libsoup-devel gsl-devel pango-devel cairo-devel double-conversion-devel gc-devel potrace-devel gtkmm3.0-devel libgdl-devel gtkspell3-devel boost-devel libxslt-devel'
 	else:
-		cmd = 'sudo apt-get install python3-gi libharfbuzz-dev libmagick++-dev libaspell-dev libgraphicsmagick1-dev libgtkspell3-3-dev libcdr-dev libvisio-dev libwpg-dev libwpd-dev libsoup2.4-dev libxslt-dev libboost-all-dev liblcms2-dev libgc-dev libdouble-conversion-dev libpotrace-dev libpangomm-2.48-dev libcairomm-1.16-dev libgtkmm-3.0-dev libgdl-3-dev libpoppler-dev libpoppler-glib-dev mm-common'
+		cmd = 'sudo apt-get install cmake libgsl-dev python3-gi libharfbuzz-dev libmagick++-dev libaspell-dev libgraphicsmagick1-dev libgtkspell3-3-dev libcdr-dev libvisio-dev libwpg-dev libwpd-dev libsoup2.4-dev libxslt-dev libboost-all-dev liblcms2-dev libgc-dev libdouble-conversion-dev libpotrace-dev libpangomm-2.48-dev libcairomm-1.16-dev libgtkmm-3.0-dev libgdl-3-dev libpoppler-dev libpoppler-glib-dev mm-common'
 	print(cmd)
 	subprocess.check_call(cmd.split())
 
